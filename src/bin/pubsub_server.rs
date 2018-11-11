5e324486f5e8ac2657a4f559eab33f2faa52ee94
Joshua Job <andrewjj20@users.noreply.github.com>
2018-11-11 13:37:22 -0800

    Added Re-Registration and Registration Timeouts #6 (#10)
    To assist in this happening, Actix was upgraded.
    in the pubsub_server, publishers is now a directory (ends in /) and a json object represents the contents.

--- src/bin/pubsub_server.rs
+++ src/bin/pubsub_server.rs
@@ -15,34 +15,56 @@ use std::net::ToSocketAddrs;
 use std::option::Option;
 use std::sync::{Arc, RwLock};
 use std::time;
+use tokio::prelude::*;
 
-type State = HashMap<String, ConnectionResponse>;
+struct ServerState {
+    publishers: HashMap<String, ConnectionResponse>,
+    publisher_timeout: time::Duration,
+}
+
+impl ServerState {
+    pub fn new(publisher_timeout: time::Duration) -> ServerState {
+        ServerState {
+            publishers: HashMap::new(),
+            publisher_timeout,
+        }
+    }
+}
+
+type State = ServerState;
 type ProtectedState = Arc<RwLock<State>>;
 
-fn handle_service_status_request(req: HttpRequest<ProtectedState>) -> HttpResponse {
+fn handle_service_status_request(req: &HttpRequest<ProtectedState>) -> HttpResponse {
     let state = req.state().read().unwrap();
-    let status = ServiceStatus { count: state.len() };
+    let status = ServiceStatus {
+        count: state.publishers.len(),
+    };
     HttpResponse::Ok().json(PubSubResponse::new(String::from("Success"), &status))
 }
 
 fn handle_add_connection(
-    req: HttpRequest<ProtectedState>,
+    req: &HttpRequest<ProtectedState>,
 ) -> Box<Future<Item = HttpResponse, Error = Error>> {
     let protected_state = Arc::clone(req.state());
     req.json()
         .from_err()
         .and_then(move |publisher: PublisherDesc| {
+            let mut state = protected_state.write().unwrap();
+            let now = time::SystemTime::now();
             let info = ConnectionResponse {
                 publisher: publisher,
                 info: ConnectionInfo {
-                    last_report: time::SystemTime::now(),
+                    last_report: now,
+                    expiration: now + state.publisher_timeout,
                 },
             };
-            let mut state = protected_state.write().unwrap();
             let name = info.publisher.name.clone();
-            state.insert(name.clone(), info);
+            state.publishers.insert(name.clone(), info);
             debug!("Added Connection \"{}\"", name);
-            Ok(HttpResponse::Ok().json(PubSubResponse::status_only(String::from("success"))))
+            Ok(HttpResponse::Ok().json(PubSubResponse::registration(
+                String::from("success"),
+                state.publisher_timeout,
+            )))
         })
         .responder()
 }
@@ -50,7 +72,7 @@ fn handle_add_connection(
 fn handle_get_connection(info: (ActixState<ProtectedState>, Path<String>)) -> HttpResponse {
     let (prot_state, name) = info;
     let state = prot_state.read().unwrap();
-    match state.get(&*name) {
+    match state.publishers.get(&*name) {
         Option::None => {
             debug!("Did not find \"{}\"", *name);
             HttpResponse::NotFound().json(PubSubResponse::status_only(String::from("not found")))
@@ -59,10 +81,30 @@ fn handle_get_connection(info: (ActixState<ProtectedState>, Path<String>)) -> Ht
     }
 }
 
-fn handle_get_connections(req: HttpRequest<ProtectedState>) -> HttpResponse {
+fn handle_get_connections(req: &HttpRequest<ProtectedState>) -> HttpResponse {
     let state = req.state().read().unwrap();
     info!("returning connections");
-    HttpResponse::Ok().json(PubSubResponse::new(String::from("success"), &*state))
+    HttpResponse::Ok().json(PubSubResponse::new(
+        String::from("success"),
+        &state.publishers,
+    ))
+}
+
+fn remove_expired_publishers(s: ProtectedState, i: time::Duration) {
+    actix::Arbiter::spawn(
+        tokio::timer::Interval::new(time::Instant::now() + i, i)
+            .for_each(move |_| {
+                let now = time::SystemTime::now();
+                s.write()
+                    .unwrap()
+                    .publishers
+                    .retain(|_k, v| v.info.expiration > now);
+                Ok(())
+            })
+            .map_err(move |e| {
+                panic!("Error: {}", e);
+            }),
+    );
 }
 
 fn socket_validator(v: String) -> Result<(), String> {
@@ -88,11 +130,30 @@ fn main() {
                 .required(true)
                 .validator(socket_validator),
         )
+        .arg(
+            Arg::with_name("publisher-timeout")
+                .short("t")
+                .takes_value(true)
+                .help("How long after registration that publishers are valid in seconds")
+                .default_value("300"),
+        )
+        .arg(
+            Arg::with_name("publisher-scan-interval")
+                .short("s")
+                .takes_value(true)
+                .help("How often are publishers scanned and possibly removed in seconds")
+                .default_value("30"),
+        )
         .get_matches();
 
+    let scan_interval = matches.value_of("publisher-scan-interval").unwrap();
+    let publisher_timeout = matches.value_of("publisher-timeout").unwrap();
     let sys = actix::System::new("example");
     let bind_info = matches.value_of("bind").unwrap();
-    let state = Arc::new(RwLock::new(State::new()));
+    let state = Arc::new(RwLock::new(State::new(time::Duration::from_secs(
+        publisher_timeout.parse().unwrap(),
+    ))));
+    let state_for_cleaner = state.clone();
 
     server::new(move || {
         let state_clone = state.clone();
@@ -100,7 +161,7 @@ fn main() {
             .resource("/status", move |r| {
                 r.method(Method::GET).h(handle_service_status_request)
             })
-            .resource("/publishers", move |r| {
+            .resource("/publishers/", move |r| {
                 r.post().f(handle_add_connection);
                 r.get().f(handle_get_connections);
             })
@@ -112,6 +173,10 @@ fn main() {
         .expect("Can not bind")
         .start();
 
+    remove_expired_publishers(
+        state_for_cleaner,
+        time::Duration::from_secs(scan_interval.parse().unwrap()),
+    );
     info!("Started server {}", bind_info);
     sys.run();
 }
--- src/bin/pubsub_server_status.rs
+++ src/bin/pubsub_server_status.rs
@@ -20,24 +20,48 @@ fn main() {
                 .takes_value(true),
         )
         .subcommand(SubCommand::with_name("service").about("Retrieves the high level status"))
+        .subcommand(SubCommand::with_name("publishers").about("Displays a list of publishers"))
         .get_matches();
 
     let base_url = matches.value_of("url").unwrap();
 
     info!("Starting Request");
-    tokio::run(
-        find_service::server_status(base_url)
-            .map_err(|err| err.to_string())
-            .and_then(|resp| {
-                info!("response received");
-                future::result(serde_json::to_string_pretty(&resp)).map_err(|err| err.to_string())
-            })
-            .then(|resp| {
-                match resp {
-                    Err(e) => println!("Error: {}", e),
-                    Ok(json) => println!("{}", json),
-                }
-                future::ok::<(), ()>(())
-            }),
-    );
+    match matches.subcommand() {
+        ("service", Some(_service_matches)) => tokio::run(
+            find_service::server_status(base_url)
+                .map_err(|err| err.to_string())
+                .and_then(|resp| {
+                    info!("response received");
+                    future::result(serde_json::to_string_pretty(&resp))
+                        .map_err(|err| err.to_string())
+                })
+                .then(|resp| {
+                    match resp {
+                        Err(e) => println!("Error: {}", e),
+                        Ok(json) => println!("{}", json),
+                    }
+                    future::ok::<(), ()>(())
+                }),
+        ),
+        ("publishers", Some(_publisers_matches)) => tokio::run(
+            find_service::get_descriptors(base_url)
+                .map_err(|err| err.to_string())
+                .and_then(|resp| {
+                    info!("response received");
+                    future::result(serde_json::to_string_pretty(&resp))
+                        .map_err(|err| err.to_string())
+                })
+                .then(|resp| {
+                    match resp {
+                        Err(e) => println!("Error: {}", e),
+                        Ok(json) => println!("{}", json),
+                    }
+                    future::ok::<(), ()>(())
+                }),
+        ),
+        (_, None) => {
+            println!("Subcommand not found");
+        }
+        _ => unreachable!(),
+    };
 }
