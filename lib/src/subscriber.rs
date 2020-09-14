6b11de9d6f021ca6579dcef32129b9c0346cfd8b
Joshua Job <josh@joshjob.com>
2020-09-13 23:47:28 -0700

    subscriber: Add Future that completes after subscription
    This assists with testing to better lock down when messages can be
    expected. It is optional and will be handled when using the Stream in
    other cases.

--- lib/src/subscriber.rs
+++ lib/src/subscriber.rs
@@ -17,15 +17,38 @@ use tokio::sync::mpsc::{self, Sender};
 use tokio::time as timer;
 use tokio_util::udp::UdpFramed;
 
+/// Future that comletes when the subscription has been acknoledge to the subscriber.
+pub struct SubscriptionComplete<'a> {
+    inner: &'a mut Subscription,
+}
+
+impl<'a> std::future::Future for SubscriptionComplete<'a> {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        debug!("Poll for subscription complete");
+        let pin = self.get_mut();
+        loop {
+            futures::ready!(pin.inner.handle_message(cx));
+            debug!("subscribed: {}", pin.inner.subscribed);
+            if pin.inner.subscribed {
+                debug!("SubscriptionComplete completed");
+                break Poll::Ready(());
+            }
+            debug!("SubscriptionComplete Retry");
+        }
+    }
+}
+
 pub struct Subscription {
     desc: PublisherDesc,
     addr: SocketAddr,
-    inner_stream: Pin<Box<dyn Stream<Item = Result<DataGram>>>>,
+    inner_stream: Pin<Box<dyn Send + Stream<Item = Result<DataGram>>>>,
     sink: Sender<DataGram>,
     generation: Generation,
     current: std::option::Option<BytesMut>,
     chunks: usize,
     received_chunks: HashSet<usize>,
+    subscribed: bool,
 }
 
 impl Subscription {
@@ -90,21 +113,22 @@ impl Subscription {
             current: None,
             chunks: 0,
             received_chunks: HashSet::new(),
+            subscribed: false,
         })
     }
 
     pub fn description(&self) -> &PublisherDesc {
         &self.desc
     }
-}
 
-impl Stream for Subscription {
-    type Item = Bytes;
+    pub fn wait_for_subscrition_complete(&mut self) -> SubscriptionComplete {
+        SubscriptionComplete { inner: self }
+    }
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
-        let pin = self.get_mut();
+    fn handle_message(&mut self, cx: &mut Context) -> Poll<()> {
         loop {
-            let message = match futures::ready!(pin.inner_stream.as_mut().poll_next(cx)) {
+            debug!("Receive one message");
+            let message = match futures::ready!(self.inner_stream.as_mut().poll_next(cx)) {
                 Some(Ok(m)) => m,
                 Some(Err(err)) => {
                     error!("Error parsing message {}", err);
@@ -114,31 +138,32 @@ impl Stream for Subscription {
             };
 
             debug!("message {:?}", message);
-            if message.1 == pin.addr {
+            if message.1 == self.addr {
                 match message.0 {
                     Message::Data(data) => {
-                        if data.generation > pin.generation {
-                            pin.generation = data.generation;
+                        if data.generation > self.generation {
+                            debug!("New data, clearing old");
+                            self.generation = data.generation;
                             let completed = data.complete_size;
                             let mut new_current = BytesMut::new();
                             new_current.resize(completed, 0);
-                            pin.current.replace(new_current);
-                            pin.chunks = completed / MAX_DATA_SIZE
+                            self.current.replace(new_current);
+                            self.chunks = completed / MAX_DATA_SIZE
                                 + if completed % MAX_DATA_SIZE == 0 { 0 } else { 1 };
-                            pin.received_chunks.clear();
+                            self.received_chunks.clear();
                         }
-                        if data.generation == pin.generation {
-                            if let Some(current) = &mut pin.current {
-                                if pin.received_chunks.insert(data.chunk) {
+                        if data.generation == self.generation {
+                            if let Some(current) = &mut self.current {
+                                if self.received_chunks.insert(data.chunk) {
                                     let offset = data.chunk * MAX_DATA_SIZE;
+                                    debug!("current length is: {}",current.len());
                                     current[offset..data.data.len()].copy_from_slice(&data.data);
                                 }
                             }
-                            if pin.received_chunks.len() == pin.chunks {
-                                if let Some(current) = pin.current.take() {
-                                    return Poll::Ready(Some(current.freeze()));
-                                }
-                            }
+                        }
+
+                        if self.received_chunks.len() == self.chunks {
+                            return Poll::Ready(());
                         }
                     }
                     Message::Acknowledgement(ack) => {
@@ -146,9 +171,10 @@ impl Stream for Subscription {
                         match ack {
                             Acknowledgement::Subscription(sub) => {
                                 debug!("Subscription Ack: {}", sub);
+                                self.subscribed = true;
                                 let timeout = sub.timeout_interval / 2;
-                                let mut sink = pin.sink.clone();
-                                let addr = pin.addr;
+                                let mut sink = self.sink.clone();
+                                let addr = self.addr;
                                 let resub = timer::delay_for(timeout).then(async move |_| {
                                     debug!("Sending Resubscription");
                                     match sink
@@ -164,6 +190,7 @@ impl Stream for Subscription {
                                 });
 
                                 tokio::spawn(resub);
+                                return Poll::Ready(());
                             }
                         }
                     }
@@ -171,6 +198,22 @@ impl Stream for Subscription {
                         //Skip unknown
                         debug!("Unknown Message: {}", message.0);
                     }
+                }
+            }
+        }
+    }
+}
+
+impl Stream for Subscription {
+    type Item = Bytes;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        let pin = self.get_mut();
+        loop {
+            futures::ready!(pin.handle_message(cx));
+            if pin.received_chunks.len() == pin.chunks {
+                if let Some(current) = pin.current.take() {
+                    return Poll::Ready(Some(current.freeze()));
                 }
             }
         }
